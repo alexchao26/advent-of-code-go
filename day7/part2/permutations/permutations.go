2dfb2e99756179fe61910c9b36222dd5f5cfe412
alexchao26 <alexchaoo@gmail.com>
2020-01-25 00:00:28 -0500

    perms code made myself using results pointer

--- day7/part2/permutations/permutations.go
+++ day7/part2/permutations/permutations.go
@@ -1,5 +1,36 @@
 package permutations
 
+// CreatePermutations docz
+func CreatePermutations(start, end int) [][]int {
+	orig := make([]int, end-start+1)
+	for i := start; i <= end; i++ {
+		orig[i-start] = i
+	}
+	results := make([][]int, 0)
+	resPointer := &results
+	perm(resPointer, orig, 0)
+
+	// fmt.Println(len(results)) // 120 = 5!
+	return results
+}
+
+func perm(resPointer *[][]int, slice []int, index int) {
+	if index == len(slice) {
+		*resPointer = append(*resPointer, slice)
+		// fmt.Println("appending", resPointer)
+	} else {
+		temp := slice[index]
+		for i := index; i < len(slice); i++ {
+			newSlice := make([]int, len(slice))
+			copy(newSlice, slice)
+			// fmt.Println(newSlice, slice)
+			newSlice[index] = newSlice[i]
+			newSlice[i] = temp
+			perm(resPointer, newSlice, index+1)
+		}
+	}
+}
+
 // CreatePermutations will return a 2D slice containing permutations from zero to the inputted int
 // func CreatePermutations(size int) [][]int {
 // 	result := make([][]int, 0)
@@ -28,32 +59,32 @@ package permutations
 // 	return result
 // }
 
-func nextPerm(p []int) {
-	for i := len(p) - 1; i >= 0; i-- {
-		if i == 0 || p[i] < len(p)-i-1 {
-			p[i]++
-			return
-		}
-		p[i] = 0
-	}
-}
+// func nextPerm(p []int) {
+// 	for i := len(p) - 1; i >= 0; i-- {
+// 		if i == 0 || p[i] < len(p)-i-1 {
+// 			p[i]++
+// 			return
+// 		}
+// 		p[i] = 0
+// 	}
+// }
 
-func getPerm(orig, p []int) []int {
-	result := append([]int{}, orig...)
-	for i, v := range p {
-		result[i], result[i+v] = result[i+v], result[i]
-	}
-	return result
-}
+// func getPerm(orig, p []int) []int {
+// 	result := append([]int{}, orig...)
+// 	for i, v := range p {
+// 		result[i], result[i+v] = result[i+v], result[i]
+// 	}
+// 	return result
+// }
 
 // CreatePermutations docz
-func CreatePermutations() [][]int {
-	orig := []int{5, 6, 7, 8, 9}
-	results := make([][]int, 0)
+// func CreatePermutations() [][]int {
+// 	orig := []int{5, 6, 7, 8, 9}
+// 	results := make([][]int, 0)
 
-	for p := make([]int, len(orig)); p[0] < len(p); nextPerm(p) {
-		// fmt.Println(getPerm(orig, p))
-		results = append(results, getPerm(orig, p))
-	}
-	return results
-}
+// 	for p := make([]int, len(orig)); p[0] < len(p); nextPerm(p) {
+// 		// fmt.Println(getPerm(orig, p))
+// 		results = append(results, getPerm(orig, p))
+// 	}
+// 	return results
+// }
