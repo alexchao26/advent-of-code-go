fb6199ea223b6eea64b747805c5db84bd7fd1a39
alexchao26 <alexchaoo@gmail.com>
2020-02-05 13:44:47 -0500

    updated LCM solution

--- day12/part2/main.go
+++ day12/part2/main.go
@@ -48,6 +48,7 @@ func main() {
 
 	// print the final least common multiple of the three number of steps
 	fmt.Println(lcm(xSteps, ySteps, zSteps))
+	fmt.Println(lcm2([]int{xSteps, ySteps, zSteps}))
 }
 
 // helper function to put the input file into a slice of strings (each elements is a line of the txt file)
@@ -176,4 +177,42 @@ func primeFactorization(num int) []int {
 		}
 	}
 	return ans
+}
+
+// Better solution to finding a least common multiple
+func lcm2(sNum []int) int {
+	ans := 1
+
+	// start iterating from the number 2
+	for i := 2; ; {
+		// booleans to track if a number has been added to the ans (LCM)
+		// and if all of the values in the slice are 1, in which case we are done
+		changeMade, allOnes := false, true
+		// iterate over all elements in the sNum slice
+		for index, num := range sNum {
+			if num%i == 0 && !changeMade {
+				changeMade = true // update this boolean flag
+				ans *= i          // increment answer
+				sNum[index] /= i  // need to use this notation because num is pass by value
+				// do not increment i
+			} else if num%i == 0 {
+				// need to divide the element, but not increment ans (same prime factor as a previous element)
+				sNum[index] /= i
+			}
+
+			// if any of the values in the slice are not one, flip the allOnes flag to false
+			// to indicate that we're not done with our outer loop
+			if num != 1 {
+				allOnes = false
+			}
+		}
+
+		// if all values in sNum are 1, we're done & can return the answer
+		if allOnes {
+			return ans
+		} else if !changeMade {
+			// if a change was not made & not all values are one, increment i
+			i++
+		}
+	}
 }
