d94081fbd94c2a49ef08fe17c4ba70b37fea9bda
Alex Chao <alexchaoo@gmail.com>
2020-08-06 19:51:52 -0400

    day18 part2 solution - not perfect but it passes :/

--- day18/part2/main.go
+++ day18/part2/main.go
@@ -3,28 +3,35 @@ package main
 import (
 	"adventofcode/util"
 	"fmt"
+	"sort"
 	"strings"
-	"time"
 )
 
 func main() {
 	input := util.ReadFile("../input.txt")
 	linesSli := strings.Split(input, "\n")
 
-	grid := make([][]string, len(linesSli)) // string might be excessive, but it will be easier to look at
-	for i, line := range linesSli {
-		grid[i] = strings.Split(line, "")
-	}
+	// generate four quadrants
+	quad1, quad2, quad3, quad4 := generateQuadrants(linesSli)
 
-	// get all the coordinates of keys. will be used as starting points for dijkstra's searches
-	keyToCoordinates := getCoordinatesOfKeys(grid)
+	// handle an individual grid, sum answers together to print
+	one := handleGrid(quad1)
+	two := handleGrid(quad2)
+	three := handleGrid(quad3)
+	four := handleGrid(quad4)
 
-	// initialize Graph
+	fmt.Printf("Sum for four robots is %v\n", one+two+three+four)
+}
+
+// Helper function to do all the things to a grid...
+func handleGrid(grid [][]string) int {
+	removeDoorsWithoutKeys(grid)
+
+	keysToCoordinates := getCoordinatesOfKeys(grid)
+
 	graph := MakeGraph()
 
-	// for every key, generate a new dijkstra grid where all distances are set to MAX SAFE INT, and the key's distance is set to 0
-	for key, sourceCoordinates := range keyToCoordinates {
-		// initialize a dijkstra grid for each key
+	for key, sourceCoordinates := range keysToCoordinates {
 		dijkstraGrid := MakeDijkstraGrid(grid, sourceCoordinates)
 
 		// step through grid until complete, handleFrontOfQueue returns true when the queue is empty...
@@ -34,8 +41,8 @@ func main() {
 		dijkstraGrid.queue = nil
 
 		// update graph for all edges from `key` to all other keys
-		for destinationKey, destinationCoordinates := range keyToCoordinates {
-			if key != destinationKey && destinationKey != "@" {
+		for otherKey, destinationCoordinates := range keysToCoordinates {
+			if key != otherKey && otherKey != "@" {
 				row, col := destinationCoordinates[0], destinationCoordinates[1]
 				// pass the key being pathed FROM and the dijkstraNode (found on the grid) of the key found
 				graph.AddEdges(key, dijkstraGrid.grid[row][col])
@@ -43,9 +50,8 @@ func main() {
 		}
 	}
 
-	dfsStartTimestamp := time.Now()
-	// first off (recursive, memoized) dfs method on graph that finds minimum distance
-	fmt.Printf("DFS result: %v\n\nFinished in %v\n\n", graph.dfsMinmumDistance(), time.Since(dfsStartTimestamp))
+	// then concurrently write back to the result chan
+	return graph.dfsMinmumDistance()
 }
 
 // get a map of the coordinates of points of interests
@@ -65,9 +71,9 @@ func getCoordinatesOfKeys(grid [][]string) map[string][2]int {
 	return pointsOfInterest
 }
 
-/********************************************************************************************
+/****************************************************************************************
 DIJKSTRA GRID Code
-*********************************************************************************************/
+*****************************************************************************************/
 
 // DijkstraGrid stores the grid itself and also the needed queue to traverse through the grid
 type DijkstraGrid struct {
@@ -169,27 +175,32 @@ func (dijk *DijkstraGrid) handleFrontOfQueue() (queueIsEmpty bool) {
 	return false
 }
 
-/********************************************************************************************
+/****************************************************************************************
 GRAPH Code
-*********************************************************************************************/
+*****************************************************************************************/
 
 // Graph stores the edges between keys by storing all paths from a particular key to all other keys
 // all cells in this 2D MAP will be a dijkstraNode because those contain all the needed data
 //   such as distance from last key, the keysNeeded to take this path
 type Graph struct {
-	keysToKeys map[string]map[string]*dijkstraNode
+	keysToKeys    map[string]map[string]*dijkstraNode
+	allKeysNeeded map[string]bool
 }
 
 // MakeGraph initializes a Graph instance and returns a pointer to it
 func MakeGraph() *Graph {
 	return &Graph{
-		keysToKeys: make(map[string]map[string]*dijkstraNode),
+		keysToKeys:    make(map[string]map[string]*dijkstraNode),
+		allKeysNeeded: make(map[string]bool),
 	}
 }
 
 // AddEdges takes in the key being pathed FROM, and the dijkstraNode of a key pathed TO
 // and adds that edge to the graph
 func (graph *Graph) AddEdges(startKey string, destinationNode *dijkstraNode) {
+	// add startKey to the allKeysNeeded hashmap
+	graph.allKeysNeeded[startKey] = true
+
 	// initialize this "row" of the map if it does not exist yet
 	if graph.keysToKeys[startKey] == nil {
 		graph.keysToKeys[startKey] = make(map[string]*dijkstraNode)
@@ -214,7 +225,7 @@ func (graph *Graph) dfsMinmumDistance() int {
 	traverse = func(entry string, keysFound map[string]bool) int {
 		shortestFromThisNode := 1<<31 - 1
 
-		cacheKey := makeCacheKey(entry, keysFound, len(graph.keysToKeys)-1)
+		cacheKey := makeCacheKey(entry, keysFound, graph.allKeysNeeded)
 
 		// cache hit
 		if cacheVal, found := cache[cacheKey]; found {
@@ -255,23 +266,18 @@ func (graph *Graph) dfsMinmumDistance() int {
 
 // helper function to generate a cache string key
 // cache string is of the form entryKey:allKeysToFind
-func makeCacheKey(entry string, keysFound map[string]bool, totalKeys int) string {
-	// TODO: move this into the graph? to avoid making it 910238190382 times
-	allKeys := make([]string, totalKeys)
-	for i := 0; i < totalKeys; i++ {
-		allKeys[i] = string(int('a') + i)
-	}
-
+func makeCacheKey(entry string, keysFound map[string]bool, allKeysNeeded map[string]bool) string {
 	cacheKey := entry
-	// generate
-	for i := 0; i < totalKeys; i++ {
-		char := string(int('a') + i)
-		if !keysFound[char] {
-			cacheKey += char
+	// generate sorted list of the keys that have NOT been found yet
+	needToFind := []string{}
+	for key := range allKeysNeeded {
+		if !keysFound[key] {
+			needToFind = append(needToFind, key)
 		}
 	}
+	sort.Strings(needToFind)
 
-	return cacheKey
+	return cacheKey + strings.Join(needToFind, "")
 }
 
 // simple helper function to make sure that all keysNeeded are in keysFound
@@ -282,4 +288,98 @@ func haveNeededKeys(keysFound, keysNeeded map[string]bool) bool {
 		}
 	}
 	return true
+}
+
+/****************************************************************************************
+INPUT SANITIZATION FUNCTIONS
+*****************************************************************************************/
+
+/* Quadrant numbers :)  mathematical because why not
+	     II  |   I
+	         |
+	 --------+---------
+	         |
+      III  |  IV
+					 |       */
+// This function is gross but it does what it says it does... returns four quadrants
+func generateQuadrants(linesSli []string) ([][]string, [][]string, [][]string, [][]string) {
+	quad1 := make([][]string, len(linesSli))
+	quad2 := make([][]string, len(linesSli))
+	quad3 := make([][]string, len(linesSli))
+	quad4 := make([][]string, len(linesSli))
+	for i, line := range linesSli {
+		quad1[i] = strings.Split(line, "")
+		quad2[i] = strings.Split(line, "")
+		quad3[i] = strings.Split(line, "")
+		quad4[i] = strings.Split(line, "")
+	}
+
+	// really only need the "@" coordinates, but might as well use this function that I alreade have..
+	keyToCoordinates := getCoordinatesOfKeys(quad1)
+
+	originRow, originCol := keyToCoordinates["@"][0], keyToCoordinates["@"][1]
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			row := originRow - 1 + i
+			col := originCol - 1 + j
+			if (row+col)%2 == 0 {
+				quad1[row][col] = "@"
+				quad2[row][col] = "@"
+				quad3[row][col] = "@"
+				quad4[row][col] = "@"
+			} else {
+				quad1[row][col] = "#"
+				quad2[row][col] = "#"
+				quad3[row][col] = "#"
+				quad4[row][col] = "#"
+			}
+		}
+	}
+	// replace origin with wall
+	quad1[originRow][originCol] = "#"
+	quad2[originRow][originCol] = "#"
+	quad3[originRow][originCol] = "#"
+	quad4[originRow][originCol] = "#"
+
+	// rescope quadrants to point to correct scope of underlying arrays
+	quad1 = quad1[:originRow+1]
+	quad2 = quad2[:originRow+1]
+	for i := range quad1 {
+		quad1[i] = quad1[i][originCol:]
+		quad2[i] = quad2[i][:originCol+1]
+	}
+	quad3 = quad3[originRow:]
+	quad4 = quad4[originRow:]
+	for i := range quad3 {
+		quad3[i] = quad3[i][:originCol+1]
+		quad4[i] = quad4[i][originCol:]
+	}
+
+	// return the four quads
+	return quad1, quad2, quad3, quad4
+}
+
+// NOTE this is a BIG assumption that will not work on all inputs, in fact it fails on a lot of the
+// note   examples... :(
+// removes doors from a quadrant if the associated key is not in the quadrant
+func removeDoorsWithoutKeys(quadrant [][]string) {
+	// put all the keys in this quadrant in a hashmap (along with walls, floors and the origin "@")
+	valuesToKeep := map[string]bool{"#": true, ".": true, "@": true}
+	for _, rowSli := range quadrant {
+		for _, cell := range rowSli {
+			if ascii := int(cell[0] - 'a'); ascii >= 0 && ascii < 26 {
+				valuesToKeep[string(ascii+'a')] = true
+			}
+		}
+	}
+
+	// iterate through the quadrant again, this time removing any capital letters
+	// who's lower case representation is NOT in the valuesToKeep hashmap
+	for row, rowSli := range quadrant {
+		for col, cell := range rowSli {
+			if lower := strings.ToLower(cell); !valuesToKeep[lower] {
+				quadrant[row][col] = "." // replace with an empty hallway
+			}
+		}
+	}
 }
