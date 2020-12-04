3293e9c3e956c4e35a45336ddd56a20a5e139661
alexchao26 <alexchaoo@gmail.com>
2020-12-04 01:21:28 -0500

    2020 day04 string parsing and validation, regexp ^ and $ killed me

--- 2020/day04/main.go
+++ 2020/day04/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"regexp"
 	"strings"
 
 	"github.com/alexchao26/advent-of-code-go/util"
@@ -27,25 +28,121 @@ func main() {
 
 func part1(input string) int {
 	parsed := parseInput(input)
-	_ = parsed
 
-	return 0
-}
-
-func part2(input string) int {
-	parsed := parseInput(input)
-	_ = parsed
-
-	return 0
-}
-
-func parseInput(input string) []int {
-	var ans []int
-
-	lines := strings.Split(input, "\n")
-	for _, l := range lines {
-		ans = append(ans, util.StrToInt(l))
+	requiredFields := []string{
+		"byr",
+		"iyr",
+		"eyr",
+		"hgt",
+		"hcl",
+		"ecl",
+		"pid",
+		// "cid", // optional
+	}
+	var ans int
+	for _, entry := range parsed {
+		hasAllFields := true
+		for _, field := range requiredFields {
+			if entry[field] == "" {
+				hasAllFields = false
+				break
+			}
+		}
+		if hasAllFields {
+			ans++
+		}
 	}
 
 	return ans
+}
+
+func part2(input string) int {
+	passports := parseInput(input)
+
+	var ans int
+	for _, entry := range passports {
+		if validateFields(entry) {
+			ans++
+		}
+	}
+
+	return ans
+}
+
+func parseInput(input string) []map[string]string {
+	var passports []map[string]string
+
+	lines := strings.Split(input, "\n\n")
+
+	for _, l := range lines {
+		passportDetails := map[string]string{}
+
+		oneLine := strings.ReplaceAll(l, "\n", " ")
+		for _, entry := range strings.Split(oneLine, " ") {
+			splitEntry := strings.Split(entry, ":")
+			passportDetails[splitEntry[0]] = splitEntry[1]
+		}
+		passports = append(passports, passportDetails)
+	}
+
+	return passports
+}
+
+func validateFields(entry map[string]string) bool {
+	byr := entry["byr"]
+	if byr > "2002" || byr < "1920" || len(byr) != 4 {
+		return false
+	}
+
+	iyr := entry["iyr"]
+	if iyr > "2020" || iyr < "2010" || len(iyr) != 4 {
+		return false
+	}
+
+	eyr := entry["eyr"]
+	if eyr > "2030" || eyr < "2020" || len(eyr) != 4 {
+		return false
+	}
+
+	hgt := entry["hgt"]
+	if hgt == "" {
+		return false
+	}
+
+	var num int
+	var unit string
+	fmt.Sscanf(hgt, "%d%s", &num, &unit)
+
+	switch unit {
+	case "cm":
+		if num < 150 || num > 193 {
+			return false
+		}
+	case "in":
+		if num < 59 || num > 76 {
+			return false
+		}
+	default:
+		return false
+	}
+
+	hcl := entry["hcl"]
+	reg := regexp.MustCompile("^#[0-9a-f]{6}$")
+	if !reg.Match([]byte(hcl)) {
+		return false
+	}
+
+	ecl := entry["ecl"]
+	reg = regexp.MustCompile("^(amb|blu|brn|gry|grn|hzl|oth)$")
+	if !reg.Match([]byte(ecl)) {
+		return false
+	}
+
+	pid := entry["pid"]
+	reg = regexp.MustCompile("^[0-9]{9}$")
+	if !reg.Match([]byte(pid)) {
+		return false
+	}
+
+	return true
 }
--- 2020/day04/main_test.go
+++ 2020/day04/main_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/alexchao26/advent-of-code-go/util"
+)
 
 var tests1 = []struct {
 	name  string
@@ -8,7 +12,20 @@ var tests1 = []struct {
 	input string
 	// add extra args if needed
 }{
-	// {"actual", ACTUAL_ANSWER, util.ReadFile("input.txt")},
+	{"example", 2, `ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm
+
+iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+hcl:#cfa07d byr:1929
+
+hcl:#ae17e1 iyr:2013
+eyr:2024
+ecl:brn pid:760753108 byr:1931
+hgt:179cm
+
+hcl:#cfa07d eyr:2025 pid:166559648
+iyr:2011 ecl:brn hgt:59in`},
+	{"actual", 233, util.ReadFile("input.txt")},
 }
 
 func TestPart1(t *testing.T) {
@@ -27,7 +44,32 @@ var tests2 = []struct {
 	input string
 	// add extra args if needed
 }{
-	// {"actual", ACTUAL_ANSWER, util.ReadFile("input.txt")},
+	{"all valid", 4, `pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719`},
+	{"all invalid", 0, `eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007`},
+	{"actual", 111, util.ReadFile("input.txt")},
 }
 
 func TestPart2(t *testing.T) {
