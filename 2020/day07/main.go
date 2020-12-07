49de18c919cdd4dcf40d8003c74fbfdf9907d03f
alexchao26 <alexchaoo@gmail.com>
2020-12-07 00:51:08 -0500

    2020 day 7 - graph traversal; parsed inputs way too slowly

--- 2020/day07/main.go
+++ 2020/day07/main.go
@@ -16,36 +16,79 @@ func main() {
 
 	if part == 1 {
 		ans := part1(util.ReadFile("./input.txt"))
-		util.CopyToClipboard(fmt.Sprintf("%v", ans))
 		fmt.Println("Output:", ans)
 	} else {
 		ans := part2(util.ReadFile("./input.txt"))
-		util.CopyToClipboard(fmt.Sprintf("%v", ans))
 		fmt.Println("Output:", ans)
 	}
 }
 
 func part1(input string) int {
-	parsed := parseInput(input)
-	_ = parsed
+	graph := parseInput(input)
+	delete(graph, "shiny gold") // shouldn't be allowed in this graph just in case
 
-	return 0
+	var outers []string
+	// traverse edges until a "shiny gold bag" is found as a content?
+	for bag := range graph {
+		if dfsFindShinyGold(graph, bag) {
+			outers = append(outers, bag)
+		}
+	}
+
+	return len(outers)
 }
 
 func part2(input string) int {
-	parsed := parseInput(input)
-	_ = parsed
+	graph := parseInput(input)
 
-	return 0
+	// subtract one for the shiny gold bag counting itself
+	return countSubbags(graph, "shiny gold") - 1
 }
 
-func parseInput(input string) []int {
-	var ans []int
+// not a fun input to parse through, there has to be a better way to do this in go...
+func parseInput(input string) map[string]map[string]int {
+	graph := map[string]map[string]int{}
+	for _, line := range strings.Split(input, "\n") {
+		split := strings.Split(line, " contain ")
+		color := split[0][:strings.Index(split[0], " bags")]
+		graph[color] = map[string]int{}
 
-	lines := strings.Split(input, "\n")
-	for _, l := range lines {
-		ans = append(ans, util.StrToInt(l))
+		for _, content := range strings.Split(split[1], ", ") {
+			if content == "no other bags." {
+				continue
+			}
+			parts := strings.Split(content, " ")
+			graph[color][parts[1]+" "+parts[2]] = util.StrToInt(parts[0])
+		}
+	}
+	return graph
+}
+
+func dfsFindShinyGold(graph map[string]map[string]int, entry string) bool {
+	// shiny gold is contained within this bag, return true & collapse call stack
+	if _, ok := graph[entry]["shiny gold"]; ok {
+		return true
 	}
 
-	return ans
+	// otherwise traverse into its edges and if it returns true, send down callstack
+	for subBags := range graph[entry] {
+		if dfsFindShinyGold(graph, subBags) {
+			return true
+		}
+	}
+
+	// otherwise shiny gold was not reached, just return false
+	return false
+}
+
+func countSubbags(graph map[string]map[string]int, entry string) int {
+	// count itself
+	bags := 1
+
+	// traverse into its subbags and add time its count
+	for subBag, count := range graph[entry] {
+		bags += count * countSubbags(graph, subBag)
+	}
+
+	return bags
 }
--- 2020/day07/main_test.go
+++ 2020/day07/main_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/alexchao26/advent-of-code-go/util"
+)
 
 var tests1 = []struct {
 	name  string
@@ -8,7 +12,16 @@ var tests1 = []struct {
 	input string
 	// add extra args if needed
 }{
-	// {"actual", ACTUAL_ANSWER, util.ReadFile("input.txt")},
+	{"example", 4, `light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.`},
+	{"actual", 119, util.ReadFile("input.txt")},
 }
 
 func TestPart1(t *testing.T) {
@@ -27,7 +40,23 @@ var tests2 = []struct {
 	input string
 	// add extra args if needed
 }{
-	// {"actual", ACTUAL_ANSWER, util.ReadFile("input.txt")},
+	{"example", 32, `light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.`},
+	{"example 2", 126, `shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.`},
+	{"actual", 155802, util.ReadFile("input.txt")},
 }
 
 func TestPart2(t *testing.T) {
