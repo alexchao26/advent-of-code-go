e48c6de3533eb6216cfc72290ac0d4d86bf82cca
alexchao26 <alexchaoo@gmail.com>
2020-12-09 18:14:24 -0500

    2020 day 09 two sum and sliding window

--- 2020/day09/main.go
+++ 2020/day09/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"math"
+	"strconv"
 	"strings"
 
 	"github.com/alexchao26/advent-of-code-go/util"
@@ -15,28 +17,112 @@ func main() {
 	fmt.Println("Running part", part)
 
 	if part == 1 {
-		ans := part1(util.ReadFile("./input.txt"))
-		util.CopyToClipboard(fmt.Sprintf("%v", ans))
+		ans := part1(util.ReadFile("./input.txt"), 25)
 		fmt.Println("Output:", ans)
 	} else {
-		ans := part2(util.ReadFile("./input.txt"))
-		util.CopyToClipboard(fmt.Sprintf("%v", ans))
+		ans := part2(util.ReadFile("./input.txt"), 25)
 		fmt.Println("Output:", ans)
 	}
 }
 
-func part1(input string) int {
-	parsed := parseInput(input)
-	_ = parsed
+func part1(input string, preambleLength int) int {
+	nums := parseInput(input)
 
-	return 0
+	for i := preambleLength; i < len(nums); i++ {
+		current := nums[i]
+
+		var good bool
+		seen := map[int]bool{}
+		for back := i - preambleLength; back < i; back++ {
+			wanted := current - nums[back]
+			if seen[wanted] {
+				good = true
+				break
+			}
+			seen[nums[back]] = true
+		}
+		if !good {
+			return nums[i]
+		}
+	}
+
+	fmt.Println("ERROR: No invalid number found for part 1")
+	return -1
 }
 
-func part2(input string) int {
-	parsed := parseInput(input)
-	_ = parsed
+// better time-complexity solution using a sliding window
+func part2(input string, preambleLength int) int {
+	numToFind := part1(input, preambleLength)
+	nums := parseInput(input)
 
-	return 0
+	var left, right, sum int
+	for right < len(nums) {
+		switch {
+		case left == right:
+			sum += nums[right]
+			right++
+		case sum > numToFind:
+			sum -= nums[left]
+			left++
+		case sum < numToFind:
+			sum += nums[right]
+			right++
+		}
+		// if sum found, and more than 1 num is in it, break
+		if sum == numToFind && left+1 != right {
+			break
+		}
+	}
+
+	// find smallest and largest values, wasted 20 minutes here when the smallest
+	// number was greater than MaxInt16...
+	smallest, largest := math.MaxInt32, -math.MaxInt32
+	for _, v := range nums[left:right] {
+		if smallest > v {
+			smallest = v
+		}
+		if largest < v {
+			largest = v
+		}
+	}
+
+	return smallest + largest
+}
+
+func part2BruteForce(input string, preambleLength int) int {
+	numToFind := part1(input, preambleLength)
+	fmt.Println(numToFind)
+	nums := parseInput(input)
+
+	var left, right int
+	for i, firstVal := range nums {
+		sum := firstVal
+		for j := i + 1; j < len(nums); j++ {
+			sum += nums[j]
+			if sum == numToFind {
+				left = i
+				right = j
+				break
+			}
+		}
+	}
+
+	if left == 0 && right == 0 {
+		fmt.Println("ERROR: No valid answer found for part 2")
+		return -1
+	}
+
+	smallest, largest := nums[left], nums[left]
+	for _, v := range nums[left:right] {
+		if smallest > v {
+			smallest = v
+		}
+		if largest < v {
+			largest = v
+		}
+	}
+
+	return smallest + largest
 }
 
 func parseInput(input string) []int {
@@ -44,7 +130,11 @@ func parseInput(input string) []int {
 
 	lines := strings.Split(input, "\n")
 	for _, l := range lines {
-		ans = append(ans, util.StrToInt(l))
+		num, err := strconv.Atoi(l)
+		if err != nil {
+			panic("parsing numbers in input " + err.Error())
+		}
+		ans = append(ans, num)
 	}
 
 	return ans
--- 2020/day09/main_test.go
+++ 2020/day09/main_test.go
@@ -1,20 +1,46 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/alexchao26/advent-of-code-go/util"
+)
+
+var exampleInput = `35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576`
 
 var tests1 = []struct {
-	name  string
-	want  int
-	input string
-	// add extra args if needed
+	name           string
+	want           int
+	input          string
+	preambleLength int
 }{
-	// {"actual", ACTUAL_ANSWER, util.ReadFile("input.txt")},
+	{"example", 127, exampleInput, 5},
+	{"actual", 15690279, util.ReadFile("input.txt"), 25},
 }
 
 func TestPart1(t *testing.T) {
 	for _, tt := range tests1 {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := part1(tt.input); got != tt.want {
+			if got := part1(tt.input, tt.preambleLength); got != tt.want {
 				t.Errorf("part1() = %v, want %v", got, tt.want)
 			}
 		})
@@ -22,18 +48,19 @@ func TestPart1(t *testing.T) {
 }
 
 var tests2 = []struct {
-	name  string
-	want  int
-	input string
-	// add extra args if needed
+	name           string
+	want           int
+	input          string
+	preambleLength int
 }{
-	// {"actual", ACTUAL_ANSWER, util.ReadFile("input.txt")},
+	{"example", 62, exampleInput, 5},
+	{"actual", 2174232, util.ReadFile("input.txt"), 25},
 }
 
 func TestPart2(t *testing.T) {
 	for _, tt := range tests2 {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := part2(tt.input); got != tt.want {
+			if got := part2(tt.input, tt.preambleLength); got != tt.want {
 				t.Errorf("part2() = %v, want %v", got, tt.want)
 			}
 		})
