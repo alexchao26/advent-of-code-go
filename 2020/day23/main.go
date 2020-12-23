afa0f4384d3a9a0cef549597b900f34c3dca3b1a
alexchao26 <alexchaoo@gmail.com>
2020-12-23 00:57:14 -0500

    2020-day23: 1250/199 - LRU cache problem, might've leaderboarded if I was a little sharper on remembering my hacky part 1 implementation

--- 2020/day23/main.go
+++ 2020/day23/main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/alexchao26/advent-of-code-go/mathutil"
+	"github.com/alexchao26/advent-of-code-go/cast"
 	"github.com/alexchao26/advent-of-code-go/util"
 )
 
@@ -15,32 +15,103 @@ func main() {
 	flag.Parse()
 	fmt.Println("Running part", part)
 
-	if part == 1 {
-		ans := part1(util.ReadFile("./input.txt"))
-		util.CopyToClipboard(fmt.Sprintf("%v", ans))
-		fmt.Println("Output:", ans)
-	} else {
-		ans := part2(util.ReadFile("./input.txt"))
-		util.CopyToClipboard(fmt.Sprintf("%v", ans))
-		fmt.Println("Output:", ans)
+	ans := crabCups(util.ReadFile("./input.txt"), part)
+	fmt.Println("Output:", ans)
+}
+
+// LLNode is a doubly linked list node
+type LLNode struct {
+	val         int
+	left, right *LLNode
+}
+
+// ~1250/ 199
+func crabCups(input string, part int) int {
+	cups := strings.Split(input, "")
+
+	currentCup := &LLNode{val: cast.ToInt(cups[0])}
+	// LRU caches are a hashmap of integers pointing to a pointer of a (doubly) LL node
+	// so finding a given node is constant time through the hash map
+	lruCacheMap := map[int]*LLNode{currentCup.val: currentCup}
+
+	// setup initial cups
+	iter := currentCup
+	for _, v := range cups[1:] {
+		iter.right = &LLNode{
+			val:  cast.ToInt(v),
+			left: iter,
+		}
+		iter = iter.right
+		lruCacheMap[iter.val] = iter
 	}
-}
+	rounds := 100 // part 1 default
 
-func part1(input string) int {
-	parsed := parseInput(input)
-	_ = parsed
-
-	return 0
-}
-
-func part2(input string) int {
-	return 0
-}
-
-func parseInput(input string) (ans []int) {
-	lines := strings.Split(input, "\n")
-	for _, l := range lines {
-		ans = append(ans, mathutil.StrToInt(l))
+	// for part 2 make rounds higher and add a bunch of extra nodes
+	if part == 2 {
+		rounds = 10000000
+		for i := 10; i <= 1000000; i++ {
+			iter.right = &LLNode{
+				val:  i,
+				left: iter,
+			}
+			iter = iter.right
+			lruCacheMap[iter.val] = iter
+		}
 	}
+
+	iter.right = currentCup
+	currentCup.left = iter
+
+	for i := 0; i < rounds; i++ {
+		valToFind := currentCup.val - 1
+
+		ignoreVals := map[int]bool{}
+		for it := currentCup.right; len(ignoreVals) < 3; it = it.right {
+			ignoreVals[it.val] = true
+		}
+
+		// if the val to find is in ignore vals or is less than smallest possible
+		// value, then loop and decrement until a valid value is found
+		for ignoreVals[valToFind] || valToFind <= 0 {
+			valToFind--
+			// loop back to highest numbered cup if value goes below zero
+			if valToFind <= 0 {
+				valToFind = len(lruCacheMap)
+			}
+		}
+
+		// next 3
+		startOfThree := currentCup.right
+		endOfThree := currentCup.right.right.right
+
+		// remove the three off the end of the current cup
+		currentCup.right = endOfThree.right
+		currentCup.right.left = currentCup
+
+		// find the LL node to attach the 3 cups to (directly from LRU cache)
+		attachAfterMe := lruCacheMap[valToFind]
+		// mash the 3 cups into the doubly LL
+		endOfThree.right = attachAfterMe.right
+		attachAfterMe.right.left = endOfThree
+		attachAfterMe.right = startOfThree
+		startOfThree.left = attachAfterMe
+
+		// move currentCup pointer
+		currentCup = currentCup.right
+	}
+
+	oneCup := lruCacheMap[1]
+
+	if part == 2 {
+		return oneCup.right.val * oneCup.right.right.val
+	}
+
+	var ans int
+	for i := 0; i < 8; i++ {
+		oneCup = oneCup.right
+		ans *= 10
+		ans += oneCup.val
+	}
+
 	return ans
 }
--- 2020/day23/main_test.go
+++ 2020/day23/main_test.go
@@ -2,37 +2,26 @@ package main
 
 import (
 	"testing"
+
+	"github.com/alexchao26/advent-of-code-go/util"
 )
 
-func Test_part1(t *testing.T) {
+func Test_crabCups(t *testing.T) {
 	tests := []struct {
 		name  string
 		input string
+		part  int
 		want  int
 	}{
-		// {"actual", util.ReadFile("input.txt"), ACTUAL_ANSWER},
+		{"example_part1", "389125467", 1, 67384529},
+		{"actual_part1", util.ReadFile("input.txt"), 1, 47382659},
+		{"example_part2", "389125467", 2, 149245887792},
+		{"actual_part2", util.ReadFile("input.txt"), 2, 42271866720},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := part1(tt.input); got != tt.want {
-				t.Errorf("part1() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_part2(t *testing.T) {
-	tests := []struct {
-		name  string
-		input string
-		want  int
-	}{
-		// {"actual", util.ReadFile("input.txt"), ACTUAL_ANSWER},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := part2(tt.input); got != tt.want {
-				t.Errorf("part2() = %v, want %v", got, tt.want)
+			if got := crabCups(tt.input, tt.part); got != tt.want {
+				t.Errorf("crabCups() = %v, want %v", got, tt.want)
 			}
 		})
 	}
