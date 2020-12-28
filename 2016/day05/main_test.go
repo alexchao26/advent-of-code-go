bfb53618638c838a67e49dfda0f59575f9f4966f
alexchao26 <alexchaoo@gmail.com>
2020-12-28 00:49:27 -0500

    fixed incorrect test input for 2015/5

--- 2016/day05/main_test.go
+++ 2016/day05/main_test.go
@@ -16,7 +16,7 @@ func Test_md5Chess(t *testing.T) {
 		{"example_part1", "abc", 1, "18f47a30"},
 		{"actual_part1", util.ReadFile("input.txt"), 1, "801b56a7"},
 		{"example_part2", "abc", 2, "05ace8e3"},
-		{"actual_part2", "abc", 2, "424a0197"},
+		{"actual_part2", util.ReadFile("input.txt"), 2, "424a0197"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
