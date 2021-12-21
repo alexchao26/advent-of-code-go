d405d523c44bee12aa773b5a5015853d18b20770
alexchao26 <alexchaoo@gmail.com>
2021-12-21 01:51:44 -0500

    2021-day19: ugly and slow but working... that was rough... also needs cleanup

---
+++ 2021/day19/main.go
@@ -0,0 +1,349 @@
+package main
+
+import (
+	_ "embed"
+	"flag"
+	"fmt"
+	"strings"
+
+	"github.com/alexchao26/advent-of-code-go/mathy"
+	"github.com/alexchao26/advent-of-code-go/util"
+)
+
+//go:embed input.txt
+var input string
+
+func init() {
+	// do this in init (not main) so test file has same input
+	input = strings.TrimRight(input, "\n")
+	if len(input) == 0 {
+		panic("empty input.txt file")
+	}
+}
+
+func main() {
+	var part int
+	flag.IntVar(&part, "part", 1, "part 1 or 2")
+	flag.Parse()
+	fmt.Println("Running part", part)
+
+	ans1, ans2 := part1(input)
+	if part == 1 {
+		util.CopyToClipboard(fmt.Sprintf("%v", ans1))
+		fmt.Println("Output:", ans1)
+	} else {
+		util.CopyToClipboard(fmt.Sprintf("%v", ans2))
+		fmt.Println("Output:", ans2)
+	}
+}
+
+func part1(input string) (part1, part2 int) {
+	scanners := parseInput(input)
+
+	// determine scanner locations by finding scanners that see 12 of the same beacons
+	// everything will be relative from scanner 0, so it is "settled" and its abs & rel coords are the same
+	settled := []scanner{scanners[0]}
+	settled[0].absoluteCoords = settled[0].relativeCoords
+	settled[0].fillAbsoluteCoordsMap()
+
+	// create helper functions that can create all the rotated versions of the seen beacons
+	// scanner 0 will have KNOWN coordinates (0,0,0)
+	// maintain a list of all UNKNOWN scanners (all but 0 at the start)
+	undetermined := scanners[1:]
+	// iterate while it has a non zero length
+	for len(undetermined) > 0 {
+		fmt.Println("undetermined:", len(undetermined))
+
+		for i, undet := range undetermined {
+			maybeUpdated, ok := findAbsoluteCoordsForScanner(undet, settled)
+			if ok {
+				// fmt.Printf("found abs for scanner %d\n", maybeUpdated.number)
+				// fmt.Printf("updated to: %+v\n", maybeUpdated)
+				settled = append(settled, maybeUpdated)
+				// remove the determined scanner from undetermined list
+				copy(undetermined[i:], undetermined[i+1:])
+				// undetermined[i] = undetermined[len(undetermined)-1]
+				undetermined = undetermined[:len(undetermined)-1]
+				// restart checks from start of undetermined list
+				break
+			}
+		}
+	}
+
+	allBeacons := map[[3]int]bool{}
+	for _, s := range settled {
+		// fmt.Printf("\n%+v\n", s)
+		for c := range s.absoluteCoordsMap {
+			allBeacons[c] = true
+		}
+	}
+
+	var furthest int
+	for i, s1 := range settled {
+		for j, s2 := range settled {
+			if i == j {
+				continue
+			}
+			manhattanDist := mathy.AbsInt(s1.x-s2.x) + mathy.AbsInt(s1.y-s2.y) + mathy.AbsInt(s1.z-s2.z)
+			if manhattanDist > furthest {
+				furthest = manhattanDist
+			}
+		}
+	}
+	return len(allBeacons), furthest
+}
+
+func part2(input string) int {
+	return 0
+}
+
+type scanner struct {
+	number            int
+	x, y, z           int
+	relativeCoords    [][3]int
+	rotations         [][][3]int
+	absoluteCoords    [][3]int
+	absoluteCoordsMap map[[3]int]bool
+}
+
+func (s *scanner) fillAbsoluteCoordsMap() {
+	s.absoluteCoordsMap = map[[3]int]bool{}
+	if len(s.absoluteCoords) == 0 {
+		panic(fmt.Sprintf("absolute coords not set for scanner %d", s.number))
+	}
+	for _, ac := range s.absoluteCoords {
+		s.absoluteCoordsMap[ac] = true
+	}
+}
+
+// create the 24 rotations given a slice of coords (3-length arrays)
+func (s *scanner) fillRotations() {
+	// facing negative x
+	posX := s.relativeCoords
+	var negX, posY, negY, posZ, negZ [][3]int
+	for _, c := range posX {
+		x, y, z := c[0], c[1], c[2]
+		// negX = append(negX, [3]int{-x, -y, z})
+		// posY = append(posY, [3]int{y, -x, -z})
+		// negY = append(negY, [3]int{-y, x, z})
+		// posZ = append(posZ, [3]int{z, -x, -y})
+		// negZ = append(negZ, [3]int{-z, -x, y})
+		negX = append(negX, [3]int{x, -y, -z})
+		posY = append(posY, [3]int{x, -z, y})
+		negY = append(negY, [3]int{-y, -z, x})
+		posZ = append(posZ, [3]int{-x, -z, -y})
+		negZ = append(negZ, [3]int{y, -z, -x})
+	}
+	sixRotations := [][][3]int{
+		posX, negX,
+		posY, negY,
+		posZ, negZ,
+	}
+
+	// apply 4 rotations around the axis that the scanner is "staring down"
+	var finalRotations [][][3]int
+	for _, rotation := range sixRotations {
+		var r2, r3, r4 [][3]int // r1 is rotation itself
+		for _, c := range rotation {
+			x, y, z := c[0], c[1], c[2]
+			// r2 = append(r2, [3]int{x, z, -y})
+			// r3 = append(r3, [3]int{x, -y, -z})
+			// r4 = append(r4, [3]int{x, -z, y})
+			r2 = append(r2, [3]int{-y, x, z})
+			r3 = append(r3, [3]int{-x, -y, z})
+			r4 = append(r4, [3]int{y, -x, z})
+		}
+		finalRotations = append(finalRotations, rotation, r2, r3, r4)
+	}
+	s.rotations = finalRotations
+}
+
+func findAbsoluteCoordsForScanner(undet scanner, settled []scanner) (maybeUpdated scanner, didUpdate bool) {
+	// for all orientations of the unknown's beacon list
+	for _, rotatedCoords := range undet.rotations {
+		// for each beacon in known list
+		for _, set := range settled {
+			for _, absCoord := range set.absoluteCoords {
+				// for each beacon in unknown list
+				for _, relativeCoord := range rotatedCoords {
+					// assume the known and unknown beacon are the same, calculate the absolute coords of the unknown's scanner coords
+					// convert all of unknown list to their absolute coords, check against known list
+					unsettledAbsoluteCoords := makeAbsoluteCoordsList(absCoord, relativeCoord, rotatedCoords)
+					// fmt.Println("\tgenerated abs coords", unsettledAbsoluteCoords)
+
+					var matchingCount int
+					// var matched [][3]int // !
+					for _, ac := range unsettledAbsoluteCoords {
+						if set.absoluteCoordsMap[ac] {
+							// matched = append(matched, ac) // !
+							matchingCount++
+						}
+					}
+
+					// if true return a true or something, modify the scanner param pointer
+					if matchingCount >= 12 {
+						// fmt.Println("matched coords", set.number, "&", undet.number, matched) // !
+						undet.relativeCoords = rotatedCoords
+						undet.absoluteCoords = unsettledAbsoluteCoords
+						undet.fillAbsoluteCoordsMap()
+						undet.x = absCoord[0] - relativeCoord[0]
+						undet.y = absCoord[1] - relativeCoord[1]
+						undet.z = absCoord[2] - relativeCoord[2]
+						return undet, true
+					}
+				}
+			}
+		}
+	}
+
+	// did not update
+	return undet, false
+}
+
+func makeAbsoluteCoordsList(absolute, relative [3]int, relativeCoords [][3]int) [][3]int {
+	// assuming absolute and relative are pointing to the same coord
+	// generate the all of the abolute coords
+
+	// diff to the scanner's coordinates, then calculate all beacons from scanner's coords
+	diff := [3]int{
+		absolute[0] - relative[0],
+		absolute[1] - relative[1],
+		absolute[2] - relative[2],
+	}
+
+	var absCoords [][3]int
+	for _, c := range relativeCoords {
+		absCoords = append(absCoords, [3]int{
+			diff[0] + c[0],
+			diff[1] + c[1],
+			diff[2] + c[2],
+		})
+	}
+
+	return absCoords
+}
+
+func parseInput(input string) (ans []scanner) {
+	for _, rawScanner := range strings.Split(input, "\n\n") {
+		var number int
+		lines := strings.Split(rawScanner, "\n")
+		_, err := fmt.Sscanf(lines[0], "--- scanner %d ---", &number)
+		if err != nil {
+			panic("parsing error " + err.Error())
+		}
+
+		var coords [][3]int
+		for _, line := range lines[1:] {
+			var x, y, z int
+			_, err := fmt.Sscanf(line, "%d,%d,%d", &x, &y, &z)
+			if err != nil {
+				panic("parsing error " + err.Error())
+			}
+			coords = append(coords, [3]int{x, y, z})
+		}
+
+		sc := scanner{
+			number:            number,
+			x:                 0,
+			y:                 0,
+			z:                 0,
+			relativeCoords:    coords,
+			absoluteCoords:    nil,
+			absoluteCoordsMap: map[[3]int]bool{},
+		}
+		sc.fillRotations()
+		ans = append(ans, sc)
+	}
+
+	return ans
+}
+
+// var exampleBeaconsList [][3]int = func() [][3]int {
+// 	var coords [][3]int
+// 	for _, l := range strings.Split(rawBeaconsList, "\n") {
+// 		var x, y, z int
+// 		fmt.Sscanf(l, "%d,%d,%d", &x, &y, &z)
+// 		coords = append(coords, [3]int{x, y, z})
+// 	}
+// 	return coords
+// }()
+
+// var rawBeaconsList = `-892,524,684
+// -876,649,763
+// -838,591,734
+// -789,900,-551
+// -739,-1745,668
+// -706,-3180,-659
+// -697,-3072,-689
+// -689,845,-530
+// -687,-1600,576
+// -661,-816,-575
+// -654,-3158,-753
+// -635,-1737,486
+// -631,-672,1502
+// -624,-1620,1868
+// -620,-3212,371
+// -618,-824,-621
+// -612,-1695,1788
+// -601,-1648,-643
+// -584,868,-557
+// -537,-823,-458
+// -532,-1715,1894
+// -518,-1681,-600
+// -499,-1607,-770
+// -485,-357,347
+// -470,-3283,303
+// -456,-621,1527
+// -447,-329,318
+// -430,-3130,366
+// -413,-627,1469
+// -345,-311,381
+// -36,-1284,1171
+// -27,-1108,-65
+// 7,-33,-71
+// 12,-2351,-103
+// 26,-1119,1091
+// 346,-2985,342
+// 366,-3059,397
+// 377,-2827,367
+// 390,-675,-793
+// 396,-1931,-563
+// 404,-588,-901
+// 408,-1815,803
+// 423,-701,434
+// 432,-2009,850
+// 443,580,662
+// 455,729,728
+// 456,-540,1869
+// 459,-707,401
+// 465,-695,1988
+// 474,580,667
+// 496,-1584,1900
+// 497,-1838,-617
+// 527,-524,1933
+// 528,-643,409
+// 534,-1912,768
+// 544,-627,-890
+// 553,345,-567
+// 564,392,-477
+// 568,-2007,-577
+// 605,-1665,1952
+// 612,-1593,1893
+// 630,319,-379
+// 686,-3108,-505
+// 776,-3184,-501
+// 846,-3110,-434
+// 1135,-1161,1235
+// 1243,-1093,1063
+// 1660,-552,429
+// 1693,-557,386
+// 1735,-437,1738
+// 1749,-1800,1813
+// 1772,-405,1572
+// 1776,-675,371
+// 1779,-442,1789
+// 1780,-1548,337
+// 1786,-1538,337
+// 1847,-1591,415
+// 1889,-1729,1762
+// 1994,-1805,1792`
---
+++ 2021/day19/main_test.go
@@ -0,0 +1,308 @@
+package main
+
+import (
+	_ "embed"
+	"testing"
+)
+
+// 0 and 1 overlap
+// scanner 1 must be at 68,-1246,-43 (relative to scanner 0)
+// 1 and 4 overlap, scanner 4 is at -20,-1133,1061 (relative to scanner 0)
+// scanner 2 must be at 1105,-1205,1229 (relative to scanner 0) and scanner 3 must be at -92,-2380,-20 (relative to scanner 0
+var example = `--- scanner 0 ---
+404,-588,-901
+528,-643,409
+-838,591,734
+390,-675,-793
+-537,-823,-458
+-485,-357,347
+-345,-311,381
+-661,-816,-575
+-876,649,763
+-618,-824,-621
+553,345,-567
+474,580,667
+-447,-329,318
+-584,868,-557
+544,-627,-890
+564,392,-477
+455,729,728
+-892,524,684
+-689,845,-530
+423,-701,434
+7,-33,-71
+630,319,-379
+443,580,662
+-789,900,-551
+459,-707,401
+
+--- scanner 1 ---
+686,422,578
+605,423,415
+515,917,-361
+-336,658,858
+95,138,22
+-476,619,847
+-340,-569,-846
+567,-361,727
+-460,603,-452
+669,-402,600
+729,430,532
+-500,-761,534
+-322,571,750
+-466,-666,-811
+-429,-592,574
+-355,545,-477
+703,-491,-529
+-328,-685,520
+413,935,-424
+-391,539,-444
+586,-435,557
+-364,-763,-893
+807,-499,-711
+755,-354,-619
+553,889,-390
+
+--- scanner 2 ---
+649,640,665
+682,-795,504
+-784,533,-524
+-644,584,-595
+-588,-843,648
+-30,6,44
+-674,560,763
+500,723,-460
+609,671,-379
+-555,-800,653
+-675,-892,-343
+697,-426,-610
+578,704,681
+493,664,-388
+-671,-858,530
+-667,343,800
+571,-461,-707
+-138,-166,112
+-889,563,-600
+646,-828,498
+640,759,510
+-630,509,768
+-681,-892,-333
+673,-379,-804
+-742,-814,-386
+577,-820,562
+
+--- scanner 3 ---
+-589,542,597
+605,-692,669
+-500,565,-823
+-660,373,557
+-458,-679,-417
+-488,449,543
+-626,468,-788
+338,-750,-386
+528,-832,-391
+562,-778,733
+-938,-730,414
+543,643,-506
+-524,371,-870
+407,773,750
+-104,29,83
+378,-903,-323
+-778,-728,485
+426,699,580
+-438,-605,-362
+-469,-447,-387
+509,732,623
+647,635,-688
+-868,-804,481
+614,-800,639
+595,780,-596
+
+--- scanner 4 ---
+727,592,562
+-293,-554,779
+441,611,-461
+-714,465,-776
+-743,427,-804
+-660,-479,-426
+832,-632,460
+927,-485,-438
+408,393,-506
+466,436,-512
+110,16,151
+-258,-428,682
+-393,719,612
+-211,-452,876
+808,-476,-593
+-575,615,604
+-485,667,467
+-680,325,-822
+-627,-443,-432
+872,-547,-609
+833,512,582
+807,604,487
+839,-516,451
+891,-625,532
+-652,-548,-490
+30,-46,-14`
+
+func Test_part1(t *testing.T) {
+	tests := []struct {
+		name   string
+		input  string
+		want1  int
+		want2  int
+		isLong bool
+	}{
+		{
+			name:  "example",
+			input: example,
+			want1: 79,   // total number of beacons
+			want2: 3621, // manhattan distance
+		},
+		{
+			name:   "actual",
+			input:  input,
+			want1:  408,
+			want2:  13348,
+			isLong: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			/*
+				// could use testing.Short() and run via go test -v -short
+				if testing.Short() {
+					t.Skip("reason")
+				}
+			*/
+			if testing.Short() {
+				t.Skip("Skipping long test, 2021/day19, O(n^ALOT)")
+			} else if tt.isLong {
+				t.Log("WARNING: Running very slow tests for 2021/day19. Disable with -short testing flag")
+			}
+			got1, got2 := part1(tt.input)
+			if got1 != tt.want1 {
+				t.Errorf("part1() = %v, want %v", got1, tt.want1)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("part2() = %v, want %v", got2, tt.want2)
+			}
+		})
+	}
+}
+
+func Test_part2(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  int
+	}{
+		{
+			name:  "example",
+			input: example,
+			want:  0,
+		},
+		// {
+		// 	name:  "actual",
+		// 	input: input,
+		// 	want:  0,
+		// },
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := part2(tt.input); got != tt.want {
+				t.Errorf("part2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+var allTheSameScanner = `--- scanner 0 ---
+-1,-1,1
+-2,-2,2
+-3,-3,3
+-2,-3,1
+5,6,-4
+8,0,7
+
+--- scanner 0 ---
+1,-1,1
+2,-2,2
+3,-3,3
+2,-1,3
+-5,4,-6
+-8,-7,0
+
+--- scanner 0 ---
+-1,-1,-1
+-2,-2,-2
+-3,-3,-3
+-1,-3,-2
+4,6,5
+-7,0,8
+
+--- scanner 0 ---
+1,1,-1
+2,2,-2
+3,3,-3
+1,3,-2
+-4,-6,5
+7,0,8
+
+--- scanner 0 ---
+1,1,1
+2,2,2
+3,3,3
+3,1,2
+-6,-4,-5
+0,7,-8`
+
+func Test_fillRotations(t *testing.T) {
+	// using a table test but there is only one case (a given example)
+	tests := []struct {
+		name  string
+		input string
+	}{
+		{input: allTheSameScanner},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			scanners := parseInput(tt.input)
+			firstScanner := scanners[0]
+			// create rotations of the first scanner
+			firstScanner.fillRotations()
+
+			// faster lookup for checking if all the original scanners are in the created rotations
+			rotationMaps := []map[[3]int]bool{}
+			for _, r := range firstScanner.rotations {
+				m := map[[3]int]bool{}
+				for _, c := range r {
+					m[c] = true
+				}
+				rotationMaps = append(rotationMaps, m)
+			}
+
+			for i, scanner := range scanners {
+				oneMapMatches := false
+				for _, m := range rotationMaps {
+					if !oneMapMatches {
+						allFound := true
+						for _, c := range scanner.relativeCoords {
+							if !m[c] {
+								allFound = false
+							}
+						}
+						if allFound {
+							oneMapMatches = true
+						}
+					}
+				}
+
+				if !oneMapMatches {
+					t.Errorf("scanner with index %d is not within createRotations() result", i)
+				}
+			}
+		})
+	}
+}
